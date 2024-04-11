60af99b403d36d5c25912d88dd6647d307403198
Nedim Jukic <nedim.jukic@uzh.ch>
2024-04-11 15:41:57 +0200

    Initialized Lobby

--- src/components/views/Lobby.tsx
+++ src/components/views/Lobby.tsx
@@ -2,35 +2,85 @@ import React, { useEffect, useState } from "react";
 import { api, handleError } from "helpers/api";
 import { Spinner } from "components/ui/Spinner";
 import { Button } from "components/ui/Button";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import "styles/views/Lobby.scss";
 import { User } from "types";
 
 const Lobby = () => {
-  // use react-router-dom's hook to access navigation, more info: https://reactrouter.com/en/main/hooks/use-navigate 
+  // use react-router-dom's hook to access navigation, more info: https://reactrouter.com/en/main/hooks/use-navigate
   const navigate = useNavigate();
+  const userId = localStorage.getItem("id");
+  const lobbyId = localStorage.getItem("lobbyId");
 
-  const logout = (): void => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("id");
-    navigate("/login");
-  };
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        //nedim-j: probably other approach needed, consider LobbyController in backend
+        const lobbyResponse = await api.post(`/lobbies/join/${lobbyId}`);
+        console.log(lobbyResponse);
+
+        const settingsResponse = await api.get(`/lobbies/settings/${lobbyId}`);
+        console.log(settingsResponse);
+
+        const friendsResponse = await api.get(`/users/${userId}/friends`);
+        console.log(friendsResponse);
+
+        await new Promise((resolve) => setTimeout(resolve, 200));
+
+      } catch (error) {
+        console.error(
+          `Something went wrong while fetching data: \n${handleError(
+            error
+          )}`
+        );
+        console.error("Details:", error);
+        alert(
+          "Something went wrong while fetching data! See the console for details."
+        );
+      }
+    }
+
+    //fetchData();
+  }, []);
 
   return (
     <BaseContainer className="lobby container">
-      <div className="buttonbar">
-        <Button style={{ flex: "8", marginRight: "1em" }} onClick={() => navigate("/menu")}>
-          Return to Menu
-        </Button>
-        <Button style={{ flex: "2" }} onClick={() => logout()}>
-          Logout
-        </Button>
-      </div>
-        
-      implement
-
+      <BaseContainer className="view">
+        <ul>
+          <li>
+            <BaseContainer className="settings">Settings</BaseContainer>
+          </li>
+          <li>
+            <BaseContainer className="main">
+              <div className="lobby-code">Code</div>
+              <div className="players">Players</div>
+              <div className="button-row">
+                Buttons
+                <Button
+                  style={{ marginTop: "10px" }}
+                  onClick={() => navigate("/game")}
+                >
+                  Start Game
+                </Button>
+                <Button
+                  style={{ marginTop: "10px" }}
+                  onClick={() => navigate("/menu")}
+                >
+                  Return to Menu
+                </Button>
+              </div>
+            </BaseContainer>
+          </li>
+          <li>
+            <BaseContainer className="friends-container">
+              <div className="friends">Friends</div>
+              <div className="username-adder">Add by Username</div>
+            </BaseContainer>
+          </li>
+        </ul>
+      </BaseContainer>
     </BaseContainer>
   );
 };
