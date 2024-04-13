557569d5535a669ebfb82a353a33fc15057ab034
Liam Kontopulos <liam.kontopulos@gmail.com>
2024-04-14 00:40:10 +0200

    Refactored code from Game to CharacterGrid and implemented (not working) functionality on top of each character

--- src/components/ui/CharacterGrid.tsx
+++ src/components/ui/CharacterGrid.tsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import { api, handleError } from "helpers/api";
 import "../../styles/ui/CharacterGrid.scss";
 import BaseContainer from "./BaseContainer";
+import { Button } from "components/ui/Button";
 import Character from "./Character";
 
-const CharacterGrid = ({CharButton}) => {
+const CharacterGrid = () => {
   const [characters, setCharacters] = useState<string[]>();
+  const [hasPicked, setHasPicked] = useState<Boolean>(false);
+  const [showInteract, setShowInteract] = useState<Boolean>(false);
+  const [characterId, setCharacterId] = useState<number>(null);
 
-  // This can also be deleted for demonstration
+  // can be deleted for simple testing purposes
   useEffect(async () => {
     try {
       const response = await api.get(`/images/random`); // frontend sends length
@@ -19,14 +23,65 @@ const CharacterGrid = ({CharButton}) => {
     }
   }, []); // Fetch data on component mount
 
-  //to make it work, delete jsx code ({..}) and only <Character/>
-  return (
+  const pickCharacter = async (id) => {
+    try {
+      setHasPicked(true);
+      const response = await api.post(`/something`, { id }); // LiamK21: change URI
+    } catch (error) {
+      alert(
+        `Something went wrong choosing your pick: \n${handleError(error)}`
+      );
+    }
+  }
+
+  /*const interactCharacter = (id, onClose) => {
+    return (
+      <BaseContainer>
+        <Button onClick={() => foldCharacter(id)}>Fold</Button>
+        <Button onClick={() => guessCharacter(id)}>Guess</Button>
+        <Button onClick={onClose}>Close</Button>
+      </BaseContainer>
+    );
+  };*/
+
+  const foldCharacter = (id) => {};
+
+  // Should probably stay here
+  const guessCharacter = (id) => {};
+
+  const handleCharacterButtonClick = () => {
+    if (!hasPicked) {
+      pickCharacter(characterId)
+    }
+    else {
+      setCharacterId(characterId);
+      setShowInteract(true);
+    }
+  };
+
+  return ( //The "1" is replaced by the actual id/idx from below
     <BaseContainer className="character-grid">
-      {characters.map(([idx, url]) => ( //change to array map
-        <Character id={idx} url={url} func={CharButton} />
-      ))}
+      <Character func={() => {setCharacterId(1); handleCharacterButtonClick()}} /> 
+      {showInteract && (
+      <dialog className="modal">
+        <div className="modal-content">
+              <Button onClick={() => foldCharacter(characterId)}>Fold</Button>
+              <Button onClick={() => guessCharacter(characterId)}>Guess</Button>
+              <Button onClick={() => setShowInteract(false)}>Close</Button>
+        </div>
+      </dialog>
+    )}
     </BaseContainer>
   );
+
+
+  /*return (
+    <BaseContainer className="character-grid">
+      {characters.map(([idx, url]) => ( //change to array map
+        <Character key={idx} url={url} func={CharButton} />
+      ))}
+    </BaseContainer>
+  );*/
 };
 
 export default CharacterGrid;
--- src/components/views/Game.tsx
+++ src/components/views/Game.tsx
@@ -10,47 +10,28 @@ import { Button } from "components/ui/Button";
 
 const Game = () => {
   // use react-router-dom's hook to access navigation, more info: https://reactrouter.com/en/main/hooks/use-navigate
-  const navigate = useNavigate();
-  const [hasPicked, setHasPicked] = useState<Boolean>(false);
-
-  const pickCharacter = async (id) => {
-    try {
-      setHasPicked(true);
-      const response = await api.post(`/something`, { id }); // LiamK21: change URI
-    } catch (error) {
-      alert(
-        `Something went wrong choosing your pick: \n${handleError(error)}`
-      );
-    }
-  }
-
-  const interactCharacter = (id) => {
-    return (
-      <BaseContainer>
-        <Button onClick={() => foldCharacter(id)}>
-          Fold
-        </Button>
-        <Button
-        onClick={() => guessCharacter(id)}>
-          Guess
-        </Button>
-      </BaseContainer>
-    )
-  }
   
-  // Can be put into "Character" or "CharacterGrid"
-  const foldCharacter = (id) => {};
-
-  // Should probably stay here
-  const guessCharacter = (id) => {};
 
   return (
     <BaseContainer className="game container">
-      <CharacterGrid CharButton={hasPicked ? interactCharacter : pickCharacter}/>
+      <CharacterGrid />
       <ChatLog />
     </BaseContainer>
   );
 
+  /*
+  return (
+  <div>
+    <Character id={id} url={url} func={() => setShowInteract(true)} />
+    {showInteract && (
+      <div className="modal">
+        <div className="modal-content">
+          <interactCharacter id={id} onClose={() => setShowInteract(false)} />
+        </div>
+      </div>
+    )}
+  </div>
+); */
 };
 
 export default Game;
