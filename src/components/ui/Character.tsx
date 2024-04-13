1064d82baec8d57b18da502094b0595fe887f6fc
Liam Kontopulos <liam.kontopulos@gmail.com>
2024-04-14 00:38:15 +0200

    Implemented characters as buttons

--- src/components/ui/Character.tsx
+++ src/components/ui/Character.tsx
@@ -3,19 +3,26 @@ import "../../styles/ui/Character.scss";
 import PropTypes from "prop-types";
 import { Button } from "components/ui/Button";
 
-// To make it work, delete <img> and key=
-const Character = ({id, url, func }) => {
+
+const Character = ({ func}) => {
+  console.log(func)
+  return (
+  <Button
+    onClick={() => func()}>
+    <div  className="character">
+    </div>
+    </Button>)
+/*
   <Button
   onClick={() => func(id)}>
   <div key={id} className="character">
     <img src={url}></img>
   </div>
-  </Button>
+  </Button>*/
 };
 
 Character.propTypes = {
-  id: PropTypes.number,
-  url: PropTypes.string,
-};
+  func: PropTypes.function
+}
 
 export default Character;
