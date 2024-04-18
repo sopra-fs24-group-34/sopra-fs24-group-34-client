ad265f27c9b5c2afc61ef354ec2850e838145453
Liam Kontopulos <liam.kontopulos@gmail.com>
2024-04-18 17:00:01 +0200

    Updated and implemented Pusher for the game chat

--- src/components/views/Game-components/PusherService.ts
+++ src/components/views/Game-components/PusherService.ts
@@ -1,19 +1,22 @@
-import Pusher from 'pusher-js';
+import Pusher, { Channel } from 'pusher-js';
 
 class PusherService {
   private pusher: Pusher;
 
+  // constructor
   constructor() {
     this.pusher = new Pusher('YOUR_PUSHER_APP_KEY', {
       cluster: 'YOUR_PUSHER_CLUSTER',
     });
   }
 
-  subscribeToChannel(channelName: string, eventName: string, callback: (data: any) => void) {
+  // subscribes to a channel and binds a function to an event
+  subscribeToChannel(channelName: string, eventName: string, methhod: Function) {
     const channel = this.pusher.subscribe(channelName);
-    channel.bind(eventName, callback);
+    channel.bind(eventName, methhod);
   }
 
+  // clean up function
   unsubscribeFromChannel(channelName: string) {
     this.pusher.unsubscribe(channelName);
   }
