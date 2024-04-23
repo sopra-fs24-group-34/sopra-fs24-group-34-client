8ef8569dbf65e2ae21aeb46657254321d4d21c70
Liam Kontopulos <liam.kontopulos@gmail.com>
2024-04-23 15:17:18 +0200

    Replaced TestCharacter with Character

--- src/components/views/Game-components/Character.tsx
+++ src/components/views/Game-components/Character.tsx
@@ -1,51 +1,17 @@
-import React, { useState, useEffect } from "react";
-import {useNavigate} from "react-router-dom";
+import React from "react";
 import "../../../styles/views/Game-components/Character.scss";
-import { api, handleError } from "helpers/api";
 import PropTypes from "prop-types";
-import PusherService from "../PusherService";
 
 // Each Character receives an id (idx in array) and an img (value in array)
-const Character = ({ id, url }) => {
-  const navigate = useNavigate();
-  const gameId = Number(localStorage.getItem("gameId"));
-  const playerId = Number(localStorage.getItem("playerId"))
-  //This (or another state) needs to be updated by the server to know that both users picked
-  const [currentRound, setCurrentRound] = useState<String>("Pick");
-  const [visibleCharacter, setvisibleCharacter] = useState<Boolean>(true);
+const TestCharacter = ({
+  character,
+  visibleCharacter,
+  pickCharacter,
+  foldCharacter,
+  guessCharacter,
+  currentRound
+}) => {
 
-  // This state depends, either we pass it as parameter or use it
-  const imageId = Number(id);
-  const pusherService = new PusherService();
-
-  useEffect(() => {
-    pusherService.subscribeToChannel(
-      `gameRound${gameId}`,
-      "round-update",
-      (response) => {
-        console.log("Received information:", response);
-        setCurrentRound(response);
-        if (currentRound === "Game-end") {
-          navigate("endscreen")
-        }
-      }
-    ); 
-
-    return () => {
-      pusherService.unsubscribeFromChannel("game");
-    };
-  }, []);
-  // Func to pick a character at the beginning
-  const pickCharacter = async () => {
-    try {
-      const send = JSON.stringify({ gameId,  playerId, imageId})
-      await api.put("/game/character/choose", {gameId, playerId, imageId} ); // LiamK21: change URI¨
-    } catch (error) {
-      alert(`Something went wrong choosing your pick: \n${handleError(error)}`);
-    }
-  };
-
-  // Func to display further functions on the character
   const interactCharacter = () => {
     if (currentRound === "Pick") {
       return (
@@ -69,27 +35,12 @@ const Character = ({ id, url }) => {
     );
   };
 
-  // Func to fold / unfold a character
-  const foldCharacter = () => {
-    setvisibleCharacter(!visibleCharacter);
-  };
-
-  // Func to guess a character
-  const guessCharacter = async () => {
-    const send = JSON.stringify({gameId, playerId, imageId})
-    const response = await api.post("/game/character/guess", {gameId, playerId, imageId});
-    if (response.data) {
-      setCurrentRound("Game-ended")
-    }
-
-  };
-
   return (
     <div
       className={`character ${visibleCharacter ? "container" : "fold"}`}
-      key={id}
+      key={character.id}
     >
-      <img className="character container img" src={url}></img>
+      <img className="character container img" src={character.url}></img>
       {visibleCharacter ? (
         <div className="character overlay">{interactCharacter()}</div>
       ) : (
@@ -99,10 +50,13 @@ const Character = ({ id, url }) => {
   );
 };
 
-Character.propTypes = {
-  id: PropTypes.number,
-  url: PropTypes.string,
-  func: PropTypes.func,
+TestCharacter.propTypes = {
+  character: PropTypes.obj,
+  visibleCharacter: PropTypes.Func,
+  pickCharacter: PropTypes.Func,
+  foldCharacter: PropTypes.Func,
+  guessCharacter: PropTypes.Func,
+  currentRound: PropTypes.String
 };
 
-export default Character;
+export default TestCharacter;
