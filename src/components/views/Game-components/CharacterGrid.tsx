8b497f6fd993a6549834a73e406353437647e5f8
Nedim Jukic <nedim.jukic@uzh.ch>
2024-04-24 23:11:28 +0200

    Fixed navigation to and display on endscreen

--- src/components/views/Game-components/CharacterGrid.tsx
+++ src/components/views/Game-components/CharacterGrid.tsx
@@ -10,7 +10,7 @@ import PusherService from "../PusherService";
 const CharacterGrid = ({ persons }) => {
   const navigate = useNavigate();
   const gameId = Number(localStorage.getItem("gameId"));
-  const userId = Number(localStorage.getItem("playerId"));
+  const playerId = Number(localStorage.getItem("playerId"));
 
   const [currentRound, setCurrentRound] = useState<String>("Pick");
   const [visibleCharacters, setVisibleCharacters] = useState<Boolean[]>(
@@ -23,12 +23,17 @@ const CharacterGrid = ({ persons }) => {
     pusherService.subscribeToChannel(
       `gameRound${gameId}`,
       "round-update",
-      (response) => {
-        console.log("Received information:", response);
+      (data) => {
+        console.log("Received information:", data);
         //nedim-j: define first in backend, what gets returned. String with state not ideal, as we probably want more info exchanged than that
         //setCurrentRound(response);
-        if (currentRound === "Game-end") {
-          navigate("endscreen");
+        if (data.guess === true) {
+          if(data.playerId === playerId) {
+            localStorage.setItem("result", "won");
+          } else {
+            localStorage.setItem("result", "lost");
+          }
+          navigate("/endscreen");
         }
       }
     );
@@ -40,10 +45,10 @@ const CharacterGrid = ({ persons }) => {
 
   const pickCharacter = async (characterId) => {
     try {
-      console.log("GAMEID", gameId, "USERID", userId, "CHARACTERID", characterId);
+      console.log("GAMEID", gameId, "PLAYERID", playerId, "CHARACTERID", characterId);
       const send = JSON.stringify({
         gameid: gameId,
-        playerid: userId,
+        playerid: playerId,
         imageid: characterId,
       });
       setCurrentRound("Guess");
@@ -69,7 +74,7 @@ const CharacterGrid = ({ persons }) => {
   const guessCharacter = async (characterId) => {
     const send = JSON.stringify({
       gameid: gameId,
-      playerid: userId,
+      playerid: playerId,
       imageid: characterId,
     });
     const response = await api.post("/game/character/guess", send);
