58a05c304fe7f09050d75de1541d2068024b3abd
Liam Kontopulos <liam.kontopulos@gmail.com>
2024-04-25 16:26:36 +0200

    Updated ModalGuess

--- src/components/views/Game-components/modalContent/ModalGuessInformation.tsx
+++ src/components/views/Game-components/modalContent/ModalGuessInformation.tsx
@@ -3,15 +3,10 @@ import PropTypes from "prop-types";
 import "styles/views/Game-components/Character.scss";
 import "styles/views/Lobby.scss";
 
-const ModalGuessInformation = ({ characterUrl, strikes }) => {
+const ModalGuessInformation = ({ strikes }) => {
   return (
     <>
-      <p>You guessed: </p>
-      <div className="character container">
-        <img className="character container img" src={characterUrl} />
-      </div>
-      <p>
-        Your guess is wrong!
+      <p>Your guess is wrong!
         <div className="empty line"></div>
         You have {strikes} strikes left
       </p>
