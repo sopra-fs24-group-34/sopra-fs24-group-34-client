97512c3764c755c7648a11aa1cf9ae492f7ee692
Liam Kontopulos <liam.kontopulos@gmail.com>
2024-04-25 13:44:59 +0200

    Updated ModalPick

--- src/components/views/Game-components/modalContent/ModalPickInformation.tsx
+++ src/components/views/Game-components/modalContent/ModalPickInformation.tsx
@@ -2,19 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import "styles/views/Game-components/Character.scss";
 
-const ModalPickInformation = ({characterUrl}) => {
+const ModalPickInformation = () => {
   return (
     <>
-      <p> You picked</p>
-      <div className="character container">
-        <img className="character container img" src={characterUrl} />
-      </div>
+      <p>
+        {" "}
+        Your pick was successful
+        <div className="empty line"> </div>
+        As soon as your opponent made his pick you can in turn ask each other
+        questions and answer them.
+      </p>
     </>
   );
 };
 
 ModalPickInformation.propTypes = {
-  characterUrl: PropTypes.String
+  characterUrl: PropTypes.String,
 };
 
 export default ModalPickInformation;
