2aa701af9d16e63e99301a9cf044ebb67aec6dee
Liam Kontopulos <liam.kontopulos@gmail.com>
2024-05-14 14:41:43 +0200

    Fixed indentation warnings

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import "../../styles/ui/Spinner.scss";
 
 export const Spinner = () => (
-    <div className="spinner-overlay">
-        <div className="loading-spinner">
-            <div/>
-            <div/>
-            <div/>
-            <div/>
-        </div>
+  <div className="spinner-overlay">
+    <div className="loading-spinner">
+      <div />
+      <div />
+      <div />
+      <div />
     </div>
+  </div>
 );
