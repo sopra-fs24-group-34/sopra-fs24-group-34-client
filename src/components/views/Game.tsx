96edfd209273553491a41e136f450ca1ac0adb5f
Liam Kontopulos <liam.kontopulos@gmail.com>
2024-05-23 12:42:30 +0200

    Issue #82: Implemented instructions and propagated modal up to Game

--- src/components/views/Game.tsx
+++ src/components/views/Game.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { api, handleError } from "helpers/api";
 import { Spinner } from "components/ui/Spinner";
+import { Button } from "components/ui/Button";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import BaseContainer from "components/ui/BaseContainer";
 import CharacterGrid from "./Game-components/CharacterGrid";
 import ChatLog from "./Game-components/ChatLog";
+import ModalDisplay from "./Game-components/modalContent/ModalDisplay";
+import ModalFirstInstructions from "./Game-components/modalContent/ModalFirstInstructions";
 import "styles/views/Game.scss";
 import "styles/views/Game-components/CharacterGrid.scss";
 import Stomp from "stompjs";
@@ -23,6 +26,10 @@ const Game = () => {
   const [isCreator, setIsCreator] = useState<boolean>(false);
   const [loading, setLoading] = useState(false);
   const [Instructions, setInstructions] = useState<String>("Pick a character");
+  const [modalState, setModalState] = useState({
+    isOpen: false,
+    content: <ModalFirstInstructions />,
+  });
 
   const fetchImages = async () => {
     const stompClient = await connectWebSocket();
@@ -45,18 +52,37 @@ const Game = () => {
 
   // useEffect to fetch images from DB
 
+  const handleOpenModal = () => {
+    setModalState({ isOpen: true, content: modalState["content"] });
+  };
+
+  const handleCloseModal = () => {
+    setModalState({ isOpen: false, content: modalState["content"] });
+  };
+
   return (
     <BaseContainer className="game container">
       <ToastContainer {...toastContainerError} />
-      <div className="instructions">
-        <h1>Current Instruction: {Instructions}</h1>
+      <div className="header">
+        <div className="instructions">
+          <h1>Current Instruction: {Instructions}</h1>
+        </div>
+        <Button className="help-button" onClick={() => handleOpenModal()}>
+          ?
+        </Button>
       </div>
       <div className="game">
         <CharacterGrid
           persons={characters}
           updateInstruction={setInstructions}
+          updateModal={setModalState}
         />
         <ChatLog updateInstruction={setInstructions} />
+        <ModalDisplay
+          isOpen={modalState.isOpen}
+          content={modalState.content}
+          handleClose={handleCloseModal}
+        />
       </div>
     </BaseContainer>
   );
